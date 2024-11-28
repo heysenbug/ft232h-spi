ff92d60785302f70e37ac259bcb2b3c47f001a5b
Abbas Shah <mashah@neuronicworks.com>
2024-11-27 22:37:12 -0500

    Add panda logic

--- spi.py
+++ spi.py
@@ -12,10 +12,10 @@ def spi_init():
     # Set up SPI settings
     cs.value = True  # Chip select inactive (high)
     spi.try_lock()
-    spi.configure(baudrate=200000, phase=0, polarity=0)
+    spi.configure(baudrate=100000, phase=0, polarity=0)
     spi.unlock()
 
-def send_data(data):
+def send_data_nrf9160(data):
     """Function to send data via SPI to nRF9160"""
     # Convert data to bytes if it’s not already
     if isinstance(data, str):
@@ -25,26 +25,67 @@ def send_data(data):
     bytes_read = bytearray(1)
     spi.try_lock()
     cs.value = False  # Activate chip select (low)
+    time.sleep(0.1)  # Small delay for CS setup
+
     spi.write(data)  # Write data to nRF
-    ret = spi.readinto(bytes_read)
+    spi.readinto(bytes_read)
+
+    time.sleep(0.1)  # Small delay before CS release
     cs.value = True   # Deactivate chip select (high)
     spi.unlock()
+
     print(f'<- TX: {int.from_bytes(data)}')
     print(f'-> RX: {int.from_bytes(bytes_read)}')
 
     return bytes_read
 
-def send_loop():
+def send_data_panda(data):
+    '''Function to get version string from panda through SPI'''
+    # Prepare the data
+    tx_buf = data
+    rx_buf = bytearray(24)
+    if isinstance(data, str):
+        tx_buf = data.encode(encoding='ascii')
+
+    # Prepare SPI for transfer
+    spi.try_lock()
+    cs.value = False
+    time.sleep(0.1)  # Small delay for CS setup
+
+    # Write/read data
+    spi.write(tx_buf)
+    time.sleep(0.01)
+    spi.readinto(rx_buf)
+
+    time.sleep(0.1)  # Small delay before CS release
+    cs.value = True
+    spi.unlock()
+
+    print(f'TX: {[hex(byte) for byte in tx_buf]}')
+    print(f'RX: {[hex(byte) for byte in rx_buf]}')
+
+
+def send_loop_nrf9160():
     print("Executing main loop")
-    buf = bytearray([0x01])
+    buf = bytearray(1)
     while True:
-        buf = send_data(buf)
-        buf[0] = (buf[0] + 1) % 256
-        time.sleep(1)
+        send_data_nrf9160(buf)
+        input('Press Any Key to send another frame')
+
+def send_loop_panda():
+    print("Executing main loop")
+    buf = "VERSION".encode(encoding='ascii')
+    while True:
+        send_data_panda(buf)
+        key = input('Press Any Key to send another frame or q to quit [continue]: ')
+        if key == 'q':
+            return
 
 
 if __name__ == '__main__':
     print("Initializing SPI")
     spi_init()
-    send_loop()
+    send_loop_panda()
+    # send_loop_nrf9160()
+    print('DONE.')
 
